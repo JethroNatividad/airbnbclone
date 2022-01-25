a74ce91ce3a8509edde01ce5470911b7502dddfc
JethroSama <kel73896@gmail.com>
2022-01-26 01:02:32 +0800

    Add header, done image

---
+++ components/Header.js
@@ -0,0 +1,11 @@
+import Image from 'next/image';
+
+const Header = () => {
+    return <header className='sticky p-5 md:px-10 shadow-md grid grid-cols-3' >
+        <div className='h-10 relative flex cursor-pointer'>
+            <Image layout='fill' src="https://links.papareact.com/qd3" objectFit='contain' objectPosition='left' alt='airbnb' />
+        </div>
+    </header>;
+};
+
+export default Header;
