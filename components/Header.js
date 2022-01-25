1709becc57a0657889da0f3bb632959b4db05fa9
JethroSama <kel73896@gmail.com>
2022-01-26 01:47:01 +0800

    style search

--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import { SearchIcon } from '@heroicons/react/outline'
 
 const Header = () => {
     return <header className='sticky p-5 md:px-10 shadow-md grid grid-cols-3' >
@@ -6,8 +7,9 @@ const Header = () => {
             <Image layout='fill' src="https://links.papareact.com/qd3" objectFit='contain' objectPosition='left' alt='airbnb' />
         </div>
         {/* search */}
-        <div>
-            <input placeholder='Search here bich' />
+        <div className='flex items-center md:border rounded-3xl py-2 px-3 md:shadow-md'>
+            <input className='outline-none bg-transparent flex-1 text-slate-400' placeholder='Search here bich' />
+            <SearchIcon className='hidden md:inline-flex h-8 bg-red-400 text-white rounded-3xl p-2 cursor-pointer' />
         </div>
 
         {/* misc */}
