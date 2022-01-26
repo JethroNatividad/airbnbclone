1480e721e0ad30ac3718340f86e3b03bccd2a519
JethroSama <kel73896@gmail.com>
2022-01-26 19:14:56 +0800

    resize button

--- components/HeroImage.js
+++ components/HeroImage.js
@@ -6,7 +6,7 @@ const HeroImage = () => {
         <Image className='w-screen object-cover' layout='fill' src="https://links.papareact.com/0fm" objectPosition='center' alt='heroimg' />
         <div className='absolute h-full w-full flex flex-col justify-center items-center'>
             <p className='text-sm sm:text-lg'>Not sure where to go? Perfect.</p>
-            <button className='bg-white py-5 px-10 text-purple-500 rounded-full shadow-md font-bold hover:shadow-lg active:scale-90 transition-all duration-150 mt-2 md:mt-5'>I'm Flexible</button>
+            <button className='bg-white py-3 px-8 text-purple-500 rounded-full shadow-md font-bold hover:shadow-lg active:scale-90 transition-all duration-150 mt-2 md:mt-5'>I'm Flexible</button>
         </div>
     </div>;
 };
