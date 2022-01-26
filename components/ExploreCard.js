eb432dc03c10eeca2479442b6aba4c40af6e444d
JethroSama <kel73896@gmail.com>
2022-01-26 19:18:01 +0800

    change margin explorer card

--- components/ExploreCard.js
+++ components/ExploreCard.js
@@ -2,8 +2,8 @@ import Image from 'next/image';
 import React from 'react';
 
 const ExploreCard = ({ img, location, distance }) => {
-    return <div className='flex items-center space-x-4 my-1 rounded-lg mx-2 hover:bg-gray-100 hover:scale-105 transition-all duration-200 ease-out cursor-pointer'>
-        <div className='h-10 w-10 relative rounded-lg '>
+    return <div className='flex items-center space-x-4 my-2 rounded-lg mx-2 hover:bg-gray-100 hover:scale-105 transition-all duration-200 ease-out cursor-pointer'>
+        <div className='h-full w-10 relative rounded-lg '>
             <Image src={img} layout='fill' alt='location' className='rounded-lg' />
         </div>
         <div>
