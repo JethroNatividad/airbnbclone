f7af688e9cc1423614c14440a48899490f1ab6f3
JethroSama <kel73896@gmail.com>
2022-01-26 18:49:32 +0800

    style explore card

--- components/ExploreCard.js
+++ components/ExploreCard.js
@@ -2,9 +2,9 @@ import Image from 'next/image';
 import React from 'react';
 
 const ExploreCard = ({ img, location, distance }) => {
-    return <div className='flex items-center space-x-4 my-4 mx-2'>
-        <div className='h-10 w-10 relative'>
-            <Image src={img} layout='fill' alt='location' />
+    return <div className='flex items-center space-x-4 my-2 rounded-lg mx-2 hover:bg-gray-100 hover:scale-105 transition-all duration-200 ease-out'>
+        <div className='h-10 w-10 relative rounded-lg '>
+            <Image src={img} layout='fill' alt='location' className='rounded-lg' />
         </div>
         <div>
             <p>{location}</p>
