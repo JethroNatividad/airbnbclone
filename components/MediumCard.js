525df8c110e0d07aac48aa7d4f590d89e4b02bb3
JethroSama <kel73896@gmail.com>
2022-02-06 19:48:56 +0800

    medium card: add hover transitions

--- components/MediumCard.js
+++ components/MediumCard.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const MediumCard = ({ img, title }) => {
     console.log(img);
-    return <div>
+    return <div className='hover:scale-105 py-5 cursor-pointer transition-transform ease-out duration-150'>
         <div className='h-80 w-80 relative mb-2'>
             <Image src={img} layout='fill' objectFit='cover' alt='title' className='rounded-lg' />
         </div>
